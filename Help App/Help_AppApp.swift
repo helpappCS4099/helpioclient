9c441543dbe41f61f4fef67c03647ef0ea8c1671
artemrakhmanov <64369654+artemrakhmanov@users.noreply.github.com>
2023-01-31 19:30:32 +0000

    Initial Commit

---
+++ Help App/Help_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Help_AppApp.swift
+//  Help App
+//
+//  Created by Artem Rakhmanov on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Help_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
